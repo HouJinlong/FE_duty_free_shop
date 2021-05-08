e493cebe9f35b0a5122e659f1f03c14f8492b607
HouJinlong <1844041598@qq.com>
2021-05-08 08:27:09 +0000

    Deploying to gh-pages from @ HouJinlong/FE_duty_free_shop@b6ba7a929abf64f3ab10930825486fe64c6fd635 🚀

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ window.appConfig = {
 	// dataUrl: 'http://121.196.121.124:8080',
 	//本地机请求数据IP地址
 	dataUrl: 'http://123.57.72.51:8080',
-	picBackground: './img/loginBackground.jpg'
+	picBackground: '/img/loginBackground.jpg'
 };
